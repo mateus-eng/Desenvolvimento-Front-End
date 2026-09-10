afac72c691d8a0b1ad78da312c5b2e5f16e3828e
mateus-eng <mateussantanamachado14@gmail.com>
2026-09-10 20:11:52 -0300

    Fix path to dados.json in carregarTarefas function

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 export async function carregarTarefas() {
-    const resposta = await fetch("../dados.json");
+
+    const resposta = await fetch("./dados.json");
 
     if (!resposta.ok) {
         const erro = new Error(`Erro HTTP: ${resposta.status}`);
